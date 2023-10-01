044f3d9b886eb6edb63069b508546b99f9f86d8e
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-01 15:59:35 +0900

    유저 인증 기능 완성
    - env 파일에 config 변수 만들고 firebase.js 에 import

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyCNfvwisynrKz_6hy0bLjF0kCjaKOWQjco",
-  authDomain: "carrot-market-b9839.firebaseapp.com",
-  databaseURL: "https://carrot-market-b9839-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "carrot-market-b9839",
-  storageBucket: "carrot-market-b9839.appspot.com",
-  messagingSenderId: "918533453591",
-  appId: "1:918533453591:web:1a9550d9ffb2364d912952"
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain:  import.meta.env.VITE_AUTH_DOMAIN,
+  databaseUvvRL: import.meta.env.VITE_DB_URL,
+  projectId: import.meta.env.VITER_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STROAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MSG_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
